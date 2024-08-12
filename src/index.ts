1a789fbc60eaae3d02704f3591a68394382d9639
oilbeater <liumengxinfly@gmail.com>
2024-08-12 13:53:00 +0000

    use hono framework
    Signed-off-by: oilbeater <liumengxinfly@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,8 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Hono } from 'hono'
+const app = new Hono()
 
-export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Welcome to Malacca!');
-	},
-} satisfies ExportedHandler<Env>;
+app.get('/', (c) => c.text('Welcome to Malacca!'))
+
+app.post('/v1/chat/completions', (c) => c.text('post completions'))
+
+export default app
