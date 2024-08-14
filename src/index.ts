fa71440cd81c2f4907c57542b126a9876ece1a7b
Mengxin Liu <liumengxinfly@gmail.com>
2024-08-14 14:59:31 +0000

    support proxy to a remote api endpoint
    Signed-off-by: Mengxin Liu <liumengxinfly@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,22 @@
-import { Hono } from 'hono'
+import { Hono, Context} from 'hono'
 const app = new Hono()
 
 app.get('/', (c) => c.text('Welcome to Malacca!'))
 
-app.post('/v1/chat/completions', (c) => c.text('post completions'))
+app.post('/v1/chat/completions', (c) => handleChat(c)).onError((err, c) => c.text('Error'))
+
+async function handleChat(c: Context) {
+  const body = await c.req.json()
+
+  const response = await fetch(c.env['endpoint'], {
+    method: 'POST',
+    body: JSON.stringify(body),
+    headers: {
+      'Content-Type': 'application/json',
+      'api-key': c.env['apiKey'],
+    }
+  })
+  return response
+}
 
 export default app
