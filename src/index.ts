a723cdb3d6334548c181119d54411913036fc052
Mengxin Liu <liumengxinfly@gmail.com>
2024-08-29 09:03:25 +0000

    add azure openai adaptor
    Signed-off-by: Mengxin Liu <liumengxinfly@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,32 @@
 import { Hono, Context } from 'hono'
+
 const app = new Hono()
+const azureOpenAI = new Hono()
 
+azureOpenAI.post('/*', handleChat)
 app.get('/', (c) => c.text('Welcome to Malacca!'))
-
-app.post('/azure-openai/:resource_name/:deployment_name/*', (c) => handleChat(c)).onError((err, c) => c.text('Error'))
+app.route('/azure-openai/:resource_name/:deployment_name', azureOpenAI).onError((err, c) => c.text(err.message, 500))
 
 async function handleChat(c: Context) {
-  const resource_name = c.req.param('resource_name')
-  const deployment_name = c.req.param('deployment_name')
-  const function_name = c.req.path.slice(`/azure-openai/${resource_name}/${deployment_name}`.length)
-  console.log(resource_name, deployment_name, c.req.path, function_name)
+  const resourceName = c.req.param('resource_name')
+  const deploymentName = c.req.param('deployment_name')
+  const functionName = c.req.path.slice(`/azure-openai/${resourceName}/${deploymentName}/`.length)
 
+  const azureEndpoint = `https://${resourceName}.openai.azure.com/openai/deployments/${deploymentName}/${functionName}`
   const body = await c.req.json()
+  const apiKey = c.req.header('api-key')
+  if (!apiKey) {
+    return c.text('api-key header is required', 401)
+  }
 
-  const response = await fetch(c.env['endpoint'], {
+  const queryParams = new URLSearchParams(c.req.query()).toString()
+  const urlWithQueryParams = `${azureEndpoint}?${queryParams}`
+  const response = await fetch(urlWithQueryParams, {
     method: 'POST',
     body: JSON.stringify(body),
     headers: {
       'Content-Type': 'application/json',
-      'api-key': c.env['apiKey'],
+      'api-key': apiKey,
     }
   })
   return response
