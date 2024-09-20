12f7508fca21017009bf65dd6c47166129dff42a
Mengxin Liu <liumengxinfly@gmail.com>
2024-09-20 08:57:29 +0000

    add test init for kv cache
    Signed-off-by: Mengxin Liu <liumengxinfly@gmail.com>

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,11 +1,17 @@
 // test/index.spec.ts
 import { env, createExecutionContext, waitOnExecutionContext, SELF } from 'cloudflare:test';
-import { describe, it, expect } from 'vitest';
+import { beforeAll, describe, it, expect } from 'vitest';
 import worker from '../src/index';
 
 // For now, you'll need to do something like this to get a correctly-typed
 // `Request` to pass to `worker.fetch()`.
 const IncomingRequest = Request<unknown, IncomingRequestCfProperties>;
+
+beforeAll(async () => {
+	// Set up Cloudflare KV
+	console.log(env)
+	await env.MALACCA_USER.put('oilbeater', import.meta.env.VITE_AZURE_API_KEY);
+});
 
 describe('Welcome to Malacca worker', () => {
 	it('responds with Welcome to Malacca! (unit style)', async () => {
