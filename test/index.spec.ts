d5978eb0dd3faf84ffdf5288d82321c97b7daf0d
oilbeater <liumengxinfly@gmail.com>
2024-09-22 22:54:18 +0800

    fix type lint
    Signed-off-by: oilbeater <liumengxinfly@gmail.com>

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,11 +1,11 @@
-// test/index.spec.ts
-import { env, createExecutionContext, waitOnExecutionContext, SELF } from 'cloudflare:test';
+/// <reference types="vite/client" />
+/// <reference path="../worker-configuration.d.ts" />
+import { env, SELF } from 'cloudflare:test';
 import { beforeAll, describe, it, expect } from 'vitest';
-import worker from '../src/index';
 
-// For now, you'll need to do something like this to get a correctly-typed
-// `Request` to pass to `worker.fetch()`.
-const IncomingRequest = Request<unknown, IncomingRequestCfProperties>;
+declare module "cloudflare:test" {
+  interface ProvidedEnv extends Env {}
+}
 
 beforeAll(async () => {
 	// Set up Cloudflare KV
